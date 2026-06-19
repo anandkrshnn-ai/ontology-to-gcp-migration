268b8a34f7c3eff3ee5f3bbdb73f73c30dac6ef2
anandkrshnn <ananda.krishnan@hotmail.com>
2026-06-19 21:35:23 +0530

    fix: shorten Spanner display name to meet GCP limits

--- terraform/modules/spanner/main.tf
+++ terraform/modules/spanner/main.tf
@@ -1,7 +1,7 @@
 resource "google_spanner_instance" "main" {
   name             = var.instance_name
   config           = "regional-${var.region}"
-  display_name     = "Ontology Graph Platform Instance"
+  display_name     = "Ontology Graph Platform"
   processing_units = var.processing_units
   project          = var.project_id
   
