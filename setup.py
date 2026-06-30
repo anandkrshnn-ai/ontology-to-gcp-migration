a3ec356ebd0b84fc5829c9e04b421ba1dbbf16e8
A B Damodaran <a.b.damodaran@accenture.com>
2026-06-30 13:09:14 +0000

    fix: package ontology bulk load modules for Dataflow workers

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="ontology_dataflow",
     version="0.1",
+    packages=find_packages(),
     install_requires=[
         "google-cloud-spanner",
         "pyyaml",
     ],
-    packages=find_packages(),
 )
